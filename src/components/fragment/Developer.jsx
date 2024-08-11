a8a0c2d6cf3a751f5f8de1f0feb4bff349e7adc8
safanone8 <safanvhora315@gmail.com>
2024-08-11 23:57:55 +0530

    updated the about info

--- src/components/fragment/Developer.jsx
+++ src/components/fragment/Developer.jsx
@@ -18,23 +18,24 @@ const Developer = () => {
                     </div>
                 </div>
                 <div className="Developer-profileDetails">
-                    <p>A Computer Science and Engineering Student at Madhuben and Bhanubhai Instiute of Technoligy, Gujarat.</p>
-                    <p>Graduating in 2023 and looking for a responsible position to gain practical knowledge</p>
+                    <p>A Computer Science and Engineering Graduate from Madhuben and Bhanubhai Instiute of Technoligy, Gujarat.</p>
+                    <p>Graduated in 2023 and looking for a responsible position to gain practical knowledge</p>
                     <p>A full-stack web developer and a Competitive coder.</p>
+                    <p>And a photographer.</p>
                     <p>I love designing fully responsive websites.</p>
                     <p>I have a keen interest in developing projects, whenever I want to learn something new.</p>
-                    <p>And a photographer.</p>
+                    
                     <div className="Card-btn">
                     <IconButton target={"_blank"}  href={"https://www.facebook.com/safan.vahora.5"} title={"safan.vahora.5"}>
                             <Facebook/>
                         </IconButton>
-                        <IconButton target={"_blank"} href={"https://twitter.com/vir8_18"}  title={"vir18"}>
+                        <IconButton target={"_blank"} href={"https://x.com/VhoraSafan37525?t=sUIlLs57mF0-tuCBhVlrRQ&s=08 "}  title={"vir18"}>
                             <Twitter/>
                         </IconButton>
-                        <IconButton target={"_blank"} href={"https://www.linkedin.com/in/safan-vhora-5a6185219/"}  title={"safan-vhora"}>
+                        <IconButton target={"_blank"} href={"https://www.linkedin.com/in/safan-vhora-9a9713266/"}  title={"safan-vhora"}>
                             <LinkedIn/>
                         </IconButton>
-                        <IconButton target={"_blank"} href={"https://www.instagram.com/damn_itz_safan/"}  title={"damn_itz_safan"}>
+                        <IconButton target={"_blank"} href={"https://www.instagram.com/safan_vhora1859/"}  title={"damn_itz_safan"}>
                             <Instagram/>
                         </IconButton>
                     </div>
